b91f1f054116c85825bf5274e5851e73b16ea483
Banu Jafar <banujafarli9@gmail.com>
2023-10-25 14:53:24 +0400

    format code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
+import { Route, Routes } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
